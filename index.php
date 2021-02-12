01290afd276fdd80839202b7268e557407ba09bc
AntonK <kantardjiev88@abv.bg>
2021-02-12 12:40:26 +0100

    first drop

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Pokemon</title>
+    <meta name="description" content="Pokedex">
+    <meta name="author" content="Anton Kantartzhiuev & Mark Hoogkamer">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content="Pokedex">
+    <meta property="og:type" content="Pokedex for pokemons">
+    <meta property="og:url" content="../img/logo.jpeg">
+    <meta property="og:image" content="img/logo.jpeg">
+
+    <link rel="icon" type="imgage/png" href="img/logo.png">
+    <link rel="apple-touch-icon" href="img/logo.png">
+
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+    <meta name="theme-col p-2or" content="#fafafa">
+
+</head>
+<body class="container-fluid main">
+
+
+<header>
+    <div class="row " id="title">
+        <h1 class="mx-auto titlecaption ">Pokemon <img src="img/logo.png" alt="Logo Pokedex "> Database</h1>
+    </div>
+</header>
+
+<main class="container">
+    <?php
+
+  const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+  function callApi (string $query) :array {
+      return json_decode(file_get_contents(API_URL .$query), true);
+  }
+
+
+
+    ?>
+    <section class="row container-fluid" id="pokedex-tpl">
+
+        <div class="col-md-6">
+            <div class="row search">
+                <label for="search"></label>
+                <form action="get">
+                    <input id="search"  type="text" name="pokemon" placeholder="Pokémon name or ID">
+                     <button type="submit" class="btn btn-warning btn-outline-dark" >Search</button>
+                </form>
+            </div>
+            <div class="row evolution">
+                <div class="col-md-3" id="evolution1"><span id="first"></span></div>
+                <div class="col-md-3" id="evolution2"><span id="second"></span></div>
+                <div class="col-md-3" id="evolution3"><span id="third"></span></div>
+            </div>
+        </div>
+        <div class="col-md-6 " id="pokemons">
+            <div class="row pokemon-img " id="pokemon-img" src="" alt="pokemon image"></div>
+            <div class="row data2" id="name"></div>
+            <div class="row data2" id="id"></div>
+            <div class="row data2" id="abilities"></div>
+
+            <table id="movestable">
+                <tr>
+                    <td>
+                        <div class="row data2" id="move1"></div>
+                    </td>
+                    <td>
+                        <div class="row data2" id="move2"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="row data2" id="move3"></div>
+                    </td>
+                    <td>
+                        <div class="row data2" id="move4"></div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+    </section>
+
+
+</main>
+<footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+            crossorigin="anonymous"></script>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+
+<!--bootstrap scripts-->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+        integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF"
+        crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+        crossorigin="anonymous"></script>
+
+<!--parallax scripts-->
+<!--so this one doesn't work either, ffs-->
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>-->
+<!--<script src="/path/to/parallax.js"></script>-->
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID.-->
+<script>
+    window.ga = function () {
+        ga.q.push(arguments)
+    };
+    ga.q = [];
+    ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto');
+    ga('set', 'anonymizeIp', true);
+    ga('set', 'transport', 'beacon');
+    ga('send', 'pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async></script>
+<!--<script src="javascript/script.js"></script>-->
+</body>
+
+</html>
---
+++ javascript/script.js
@@ -0,0 +1,104 @@
+(() => {
+
+    let input = document.getElementById("search");
+    let pressSearch = document.getElementById("pressSearch");
+    let evolution1 = document.getElementById("first");
+    let evolution2 = document.getElementById("second");
+    let evolution3 = document.getElementById("third");
+
+    function allEvolutions(search) {
+        fetch(search)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (pokemon) {
+
+                let image = pokemon.sprites.front_default;
+                let id = parseInt(pokemon.id);
+                let name = pokemon.forms[0].name;
+
+                document.getElementById("pokemon-img").innerHTML = "<img src='" + image + "' id='mainimg'>";
+                document.getElementById("id").innerText = "Id: " + id;
+                document.getElementById("name").innerText = "Name: " + name;
+
+                let allMoves = pokemon.moves;
+
+                document.getElementById("move1").innerText = allMoves[0].move.name;
+                document.getElementById("move2").innerText = allMoves[1].move.name;
+                document.getElementById("move3").innerText = allMoves[2].move.name;
+                document.getElementById("move4").innerText = allMoves[3].move.name;
+
+                fetch(pokemon.species.url)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (species) {
+                        fetch(species.evolution_chain.url)
+                            .then(function (response) {
+                                return response.json();
+                            })
+                            .then(function (evolution) {
+
+                                if (evolution.chain.evolves_to.length === 1) {
+                                    one = "" + evolution.chain.species.url.replace('-species', '') + "";
+                                    two = "" + evolution.chain.evolves_to[0].species.url.replace('-species', '') + "";
+                                    if (evolution.chain.evolves_to[0].evolves_to[0] !== undefined) {
+                                        three = "" + evolution.chain.evolves_to[0].evolves_to[0].species.url.replace('-species', '') + "";
+                                    }
+
+                                    fetch(one)
+                                        .then(function (response) {
+                                            return response.json();
+                                        })
+                                        .then(function (firstEvo) {
+                                            evolution1.innerHTML = "<img src='" + firstEvo.sprites.front_default + "' id='evimg1'>";
+                                        });
+
+                                    fetch(two)
+                                        .then(function (response) {
+                                            return response.json();
+                                        })
+                                        .then(function (secondEvo) {
+                                            evolution2.innerHTML = "<img src='" + secondEvo.sprites.front_default + "' id='evimg2'>";
+                                        });
+
+                                    fetch(three)
+                                        .then(function (response) {
+                                            return response.json();
+                                        })
+                                        .then(function (thirdEvo) {
+                                            if (evolution.chain.evolves_to[0].evolves_to[0] === undefined) {
+                                                evolution3.innerHTML = "<img src='img/backS.png'" + "' id='evimg3'>";
+                                            } else {
+                                                evolution3.innerHTML = "<img src='" + thirdEvo.sprites.front_default + "' id='evimg3'>";
+                                            }
+                                        });
+                                }
+                            });
+                    });
+            });
+    }
+
+
+    document.getElementById("first").addEventListener("click", function () {
+        allEvolutions(one);
+    });
+
+    document.getElementById("second").addEventListener("click", function () {
+        allEvolutions(two);
+    });
+
+    document.getElementById("third").addEventListener("click", function () {
+        allEvolutions(three);
+    });
+
+    pressSearch.addEventListener("click", function () {
+        let search = "https://pokeapi.co/api/v2/pokemon/" + input.value;
+        allEvolutions(search.toLowerCase());
+    });
+
+
+    allEvolutions("https://pokeapi.co/api/v2/pokemon/1");
+
+
+})();
